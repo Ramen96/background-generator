993e267d15448169c7dfcc89e44afbe31750f679
eric <eric.worsfold@gmail.com>
2024-01-02 21:41:37 -0500

    Adding starting projecet

---
+++ script.js
@@ -0,0 +1,41 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("Random");
+
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+const random_hex_color_code = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+  };
+
+function genRanHex() {
+    body.style.background = "linear-gradient(to right, "
+    + random_hex_color_code()
+    + ", "
+    + random_hex_color_code()
+    ")";
+        
+    css.textContent = body.style.background + ";";
+    console.log(genRanHex(6));
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", genRanHex);
+
+
+genRanHex();  // Call function so color inputs match the background generated 
+                // and Display the initial CSS linear gradient property 
+                // on the first page load. 
